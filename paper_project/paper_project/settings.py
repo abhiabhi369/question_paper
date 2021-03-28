366b251b0a3ea31ca4ba217ce4f151eba893fe31
abhilash <abhilashgangula370@gmail.com>
2021-03-28 14:15:34 +0530

    hello

--- paper_project/paper_project/settings.py
+++ paper_project/paper_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4!ge3xrs#$z4ubw=d*f)f($3l30_+he%f=92uq&nw^q-f9ty87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
